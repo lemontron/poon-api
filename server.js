a962c5604d29049f4c8029bf9c7cccc5d6541ec7
James <jamesloper@gmail.com>
2026-08-20 17:07:56 -0400

    wokr on PWA conversion

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import { WebApp } from 'meteor/webapp';
 import express from 'express';
 
 export const api = express();
+const app = express();
 
 api.use((err, req, res, next) => {
 	res.status(err.status || 500);
 	res.json({'error': err.message});
 });
 
-WebApp.rawConnectHandlers.use('/api', api);
+app.use('/api', api);
+
+WebApp.rawConnectHandlers.use((req, res, next) => {
+	if (req.url !== '/api' && !req.url.startsWith('/api/')) return next();
+	return app(req, res, next);
+});
